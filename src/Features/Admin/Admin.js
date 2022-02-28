b21cfae7f329deefba891de39675da665564ac13
Ravinder Singh <ravinderz@outlook.com>
2022-02-28 22:59:40 +0530

    added tailwind css

--- src/Features/Admin/Admin.js
+++ src/Features/Admin/Admin.js
@@ -2,12 +2,11 @@ import { Button } from '@mui/material'
 import React, { useState } from 'react'
 import * as XLSX from 'xlsx';
 
-import { DataGrid } from '@mui/x-data-grid';
-
 const Admin = () => {
 
     let [tableData, setTableData] = useState([]);
-    let [columnData, setColumnData] = useState([]);
+    let [objProperties, setObjProperties] = useState([]);
+    let [tableHeaders, setTableHeaders] = useState([]);
 
     const handleChange = (e) => {
         const file = e.target.files[0];
@@ -26,40 +25,33 @@ const Admin = () => {
         reader.readAsBinaryString(file);
     }
 
+    const getObjProps = (headers) => {
+        const temp = [];
+        headers.map(element => {
+            let val = element.toLowerCase();
+            val = val.split(" ").join('_');
+            console.log(val);
+            temp.push(val);
+        });
+        console.log(temp);
+        return temp;
+    }
+
     const processData = (csvDataString) => {
         const csvRows = csvDataString.split(/\r\n|\n/);
         const headers = csvRows[0].split(/,/);
-
         const list = [];
-
+        const temp = getObjProps(headers);
         for (let i = 1; i < csvRows.length; i++) {
             let cols = csvRows[i].split(',');
             let obj = {};
             for (let j = 0; j < cols.length; j++) {
-                obj[headers[j]] = cols[j];
+                obj[temp[j]] = cols[j];
             }
             list.push(obj);
         }
-        console.log(list);
         setTableData(list);
-        setColumnData(generateColumnData(headers));
-
-    }
-
-    const generateColumnData = (header) => {
-
-        let list = [];
-
-        header.forEach(elm => {
-            list.push({
-                field: elm,
-                headerName: elm,
-                sortable: true,
-                width: 200,
-            })
-        });
-
-        return list;
+        setTableHeaders(headers);
     }
 
     const generateRowId = () => {
@@ -83,14 +75,27 @@ const Admin = () => {
                     </Button>
                 </label>
             </div>
-            <div style={{ height: 400, width: '100%' }}>
-                <DataGrid
-                    rows={tableData}
-                    columns={columnData}
-                    pageSize={20}
-                    rowsPerPageOptions={[20]}
-                    getRowId={generateRowId}
-                />
+            <div className='flex justify-center m-4'>
+                <table className='border border-solid'>
+                    <thead className='divide-y bg-slate-500 text-amber-50'>
+                        <tr>
+                            {tableHeaders.map((e, i) => {
+                                return <th key={i} className='p-4'>{e}</th>
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableData.map((e, i) => {
+                            return (<tr key={generateRowId()} className='even:bg-slate-200'>
+                                <td className='p-2 text-sm lowercase'>{e.serial_number}</td>
+                                <td className='p-2 text-sm lowercase'>{e.employee_markme}</td>
+                                <td className='p-2 text-sm lowercase'>{e.description}</td>
+                                <td className='p-2 text-sm lowercase'>{e.company_name}</td>
+                                <td className='p-2 text-sm lowercase'>{e.leave}</td>
+                            </tr>)
+                        })}
+                    </tbody>
+                </table>
             </div>
         </>
 
